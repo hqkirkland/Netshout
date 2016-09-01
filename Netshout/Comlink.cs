769f07e10df102260c31e65684752eff5098d470
hqkirkland <hunter.kirkland@live.com>
2016-09-01 11:38:58 -0500

    Initial commit.

---
+++ Netshout/Comlink.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netshout
+{
+    public class Comlink
+    {
+        public Socket ClientSocket;
+        public byte[] IncomingBuffer;
+        public bool Connecting;
+        public bool Connected;
+        public bool Ready;
+
+        public String IpAddr;
+        public String Username;
+        public String Password;
+        public String StreamId;
+        public String Format;
+        public int Bitrate;
+
+        public int BufferSize;
+
+        public Comlink(String ipAddr, int port, String user, String password, String sid, String format, int bitrate)
+        {
+            ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse(ipAddr);
+
+            // Server is listening.
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            this.Username = user;
+            this.IpAddr = ipAddr;
+            this.Password = password;
+            this.StreamId = sid;
+            this.Format = format;
+            this.Bitrate = bitrate;
+
+            // Connect to the server
+            Connected = false;
+            Ready = false;
+            Connecting = true;
+
+            ClientSocket.BeginConnect(ipEndPoint, new AsyncCallback(OnConnect), null);
+        }
+
+        private void OnConnect(IAsyncResult ar)
+        {
+            Connecting = false;
+            Connected = true;
+            IncomingBuffer = new byte[3];
+            ClientSocket.EndConnect(ar);
+            //Send the version/startup packet to the server
+            byte[] VersionBytes = { 0x5A, 0x0, 0x10, 0x009, 0x0, 0x03, 0x32, 0x2e, 0x31, 0x0 };
+            Send(VersionBytes);
+
+            ClientSocket.Receive(IncomingBuffer);
+            ClientSocket.Receive(IncomingBuffer);
+
+            // Receiving the cipher, sending auth blob.
+            IncomingBuffer = new byte[16];
+            ClientSocket.Receive(IncomingBuffer);
+            String CipherKey = Encoding.Default.GetString(IncomingBuffer).Trim(new char[] { (char)(0) });
+            CipherKey = CipherKey.Replace("ACK:", "");
+            String AuthBlob = "2.1:" + StreamId + ":" + XTEA.Encrypt(Username, CipherKey).ToLower() + ":" + XTEA.Encrypt(Password, CipherKey).ToLower();
+            Packet AuthPacket = new Packet(0x10, 0x001, AuthBlob);
+            Send(AuthPacket.Serialize());
+
+            IncomingBuffer = new byte[48];
+            ClientSocket.Receive(IncomingBuffer);
+            String AuthResponse = Encoding.Default.GetString(IncomingBuffer, 6, 3).Trim();
+
+            if (AuthResponse == "NAK")
+            {
+                throw new Exception(Encoding.Default.GetString(IncomingBuffer, 6, 24).Trim());
+            }
+
+            Ready = true;
+
+            Packet MimePacket = new Packet(0x10, 0x040, Format);
+            Send(MimePacket.Serialize());
+
+            IncomingBuffer = new byte[32];
+            ClientSocket.Receive(IncomingBuffer);
+            String MimeResponse = Encoding.Default.GetString(IncomingBuffer, 6, 3);
+
+            if (MimeResponse == "NAK")
+            {
+                throw new Exception(Encoding.Default.GetString(IncomingBuffer, 6, 25).Trim());
+            }
+
+            Packet BroadcastSetup = new Packet(0x10, 0x002, Bitrate + ":" + Bitrate);
+            Send(BroadcastSetup.Serialize());
+
+            IncomingBuffer = new byte[32];
+            ClientSocket.Receive(IncomingBuffer);
+            String BitrateResponse = Encoding.Default.GetString(IncomingBuffer, 6, 3);
+
+            if (BitrateResponse == "NAK")
+            {
+                throw new Exception(Encoding.Default.GetString(IncomingBuffer, 6, 25).Trim());
+            }
+
+            Packet bufferSetup = new Packet(0x10, 0x003, BufferSize + ":0");
+            Send(bufferSetup.Serialize());
+
+            IncomingBuffer = new byte[32];
+            ClientSocket.Receive(IncomingBuffer);
+            String BufferResponse = Encoding.Default.GetString(IncomingBuffer, 6, 3);
+
+            if (BitrateResponse == "ACK")
+            {
+                BufferResponse = Encoding.Default.GetString(IncomingBuffer, 10, 3);
+                BufferSize = int.Parse(BufferResponse);
+            }
+
+            else
+            {
+                throw new Exception(Encoding.Default.GetString(IncomingBuffer, 6, 25).Trim());
+            }
+
+            Packet FrameSize = new Packet(0x10, 0x008, "16377:0");
+            Send(FrameSize.Serialize());
+
+            IncomingBuffer = new byte[32];
+            ClientSocket.Receive(IncomingBuffer);
+            String FrameResponse = Encoding.Default.GetString(IncomingBuffer, 6, 3);
+
+            if (FrameResponse == "NAK")
+            {
+                throw new Exception(Encoding.Default.GetString(IncomingBuffer, 6, 25));
+            }
+
+            ClientSocket.BeginReceive(IncomingBuffer, 0, IncomingBuffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+        }
+
+        public void Send(byte[] OutBuffer)
+        {
+            try
+            {
+                // Fill the info for the message to be sent.
+                ClientSocket.BeginSend(OutBuffer, 0, OutBuffer.Length, SocketFlags.None, new AsyncCallback(OnSend), null);
+            }
+
+            catch (Exception)
+            {
+                ClientSocket.Shutdown(SocketShutdown.Both);
+                ClientSocket.Disconnect(false);
+                ClientSocket.Dispose();
+            }
+        }
+
+        private void OnSend(IAsyncResult ar)
+        {
+            ClientSocket.EndSend(ar);
+        }
+
+        private void OnReceive(IAsyncResult ar)
+        {
+            try
+            {
+                ClientSocket.EndReceive(ar);
+
+                if (IncomingBuffer != null)
+                {
+
+                }
+
+                IncomingBuffer = new byte[64];
+                ClientSocket.BeginReceive(IncomingBuffer, 0, IncomingBuffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+            }
+
+            catch (Exception)
+            {
+                ClientSocket.Shutdown(SocketShutdown.Both);
+                ClientSocket.Disconnect(false);
+                ClientSocket.Dispose();
+            }
+        }
+    }
+}
---
+++ Netshout/XTEA.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Netshout
+{
+    class XTEA
+    {
+        public static string Encrypt(String dataStr, String keyStr)
+        {
+            byte[] DataStrBytes = Encoding.Default.GetBytes(dataStr);
+
+            // Declare new var here.
+            byte[] DataBytes;
+
+            if (DataStrBytes.Length % 8 > 0)
+            {
+                /* If the DataStrBytes length isn't a multiple of 8, it will trigger this.
+                 * The following will take the remainder of the operation DataStrBytes.Length
+                 * / 8, subtract that from 8, and create a byte of that result + the current
+                 * size.
+                 */
+                DataBytes = new byte[DataStrBytes.Length + (8 - (DataStrBytes.Length % 8))];
+            }
+
+            else
+            {
+                DataBytes = new byte[DataStrBytes.Length];
+            }
+
+            /* Since we cant extend byte arrays (at least, to my knowledge :), we copy
+             * the filled array to the new, properly-sized one.
+             */
+
+            DataStrBytes.CopyTo(DataBytes, 0);
+
+            Console.WriteLine("DataBytes.Length: " + DataBytes.Length);
+            Console.WriteLine("DataBytes[]: " + BitConverter.ToString(DataBytes));
+
+            UInt32[] DataArr = new UInt32[DataBytes.Length / 4];
+
+            for (int i = 0; i < DataArr.Length; i++)
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    // This ensures that the bytes are read correctly on little endian machines.
+                    Array.Reverse(DataBytes, i * 4, 4);
+                }
+
+                /* This for loop runs through our array and converts each byte to uint32,
+                 * Since every uint32 is four bytes, the index that bitConverter is going
+                 * to start reading for the next uint is i * 4. 
+                 */
+                DataArr[i] = BitConverter.ToUInt32(DataBytes, i * 4);
+            }
+
+            // Key
+
+            Console.Write("KEY: ");
+
+            /* We follow the same steps as above, but the key is always going to be 4 uint32s,
+             * and, therefore, we can move without the length checks since it will always
+             * be 16 bytes long.
+             */
+
+            byte[] KeyBytes = new byte[16];
+            Encoding.Default.GetBytes(keyStr).CopyTo(KeyBytes, 0);
+
+            Console.WriteLine("keyBytes[]: " + BitConverter.ToString(KeyBytes));
+
+            UInt32[] keyArr = new UInt32[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(KeyBytes, i * 4, 4);
+                }
+
+                keyArr[i] = BitConverter.ToUInt32(KeyBytes, i * 4);
+            }
+
+            Console.WriteLine(DataArr[1].ToString() + " " + DataArr[0].ToString());
+            Console.WriteLine(keyArr[0].ToString() + " " + keyArr[1].ToString() + " " + keyArr[2].ToString() + " " + keyArr[3].ToString());
+
+            uint Y = DataArr[0];
+            uint Z = DataArr[1];
+            uint Sum = 0;
+            uint Delta = 0x9E3779B9;
+            uint n = 32;
+
+            while (n-- > 0)
+            {
+                Y += (Z << 4 ^ Z >> 5) + Z ^ Sum + keyArr[Sum & 3];
+                Sum += Delta;
+                Z += (Y << 4 ^ Y >> 5) + Y ^ Sum + keyArr[Sum >> 11 & 3];
+            }
+
+            DataArr[0] = Y;
+            DataArr[1] = Z;
+
+            return DataArr[0].ToString("X") + DataArr[1].ToString("X");
+        }
+    }
+}
